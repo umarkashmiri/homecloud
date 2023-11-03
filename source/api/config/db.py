b19ed0879edb807d267d0522ab366837ae493a48
umar <umarkashmiri@gmail.com>
2023-11-03 20:09:52 +0800

    docker compose setup

--- source/api/config/db.py
+++ source/api/config/db.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
 
-con = MongoClient()
+con = MongoClient("mongodb://homecloud:saTiger123@home-cloud-db")
+#con = MongoClient("mongodb://homecloud:saTiger123@localhost")
